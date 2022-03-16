cc9ace116ad24a985430fdd07244612f02dfe628
ddgg9511@naver.com <ddgg9511@naver.com>
2022-03-16 12:11:59 +0900

    회원가입 테스트

--- book/src/test/java/com/example/book/controller/UserControllerTest.java
+++ book/src/test/java/com/example/book/controller/UserControllerTest.java
@@ -2,22 +2,30 @@ package com.example.book.controller;
 
 import com.example.book.domain.User;
 import com.example.book.dto.UserSaveRequestData;
+import com.example.book.repository.UserRepository;
 import com.example.book.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
     @MockBean
@@ -26,17 +34,46 @@ class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    public void signUp() throws Exception{
-        UserSaveRequestData 박성은 = UserSaveRequestData.builder()
-                .name("박성은")
-                .password("123")
-                .email("bbung@naver.com").build();
+    private static final String NAME = "테스트 이름";
+    private static final String EMAIL = "test@email.com";
+    private static final String PASSWORD = "1234abcd*";
+    private static final String PICTURE = "테스트 사진";
 
-        when(userService.signUp(any())).thenReturn(박성은.toEntity());
+    @Autowired
+    private UserRepository userRepository;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-        mockMvc.perform(post("/user",박성은))
-                .andExpect(status().isOk());
+    @Nested
+    @DisplayName("사용자 회원가입 요청은")
+    class Describe_signUp {
+        UserSaveRequestData userSaveRequestData;
+
+        @Nested
+        @DisplayName("사용자 회원가입 정보가 주어진다면")
+        class Context_with_user_data {
+            @BeforeEach
+            void setUp() {
+                userSaveRequestData = getUserSaveData("new");
+            }
+
+            @Test
+            @DisplayName("사용자를 저장하고 Created를 응답한다.")
+            void it_response_status_created() throws Exception {
+                mockMvc.perform(post("/user")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(userSaveRequestData)))
+                        .andExpect(status().isCreated())
+                        .andDo(print());
+            }
+        }
+    }
+
+    private UserSaveRequestData getUserSaveData(String prefix) {
+        return UserSaveRequestData.builder()
+                .email(prefix + EMAIL)
+                .name(NAME)
+                .password(PASSWORD)
+                .build();
     }
 }
