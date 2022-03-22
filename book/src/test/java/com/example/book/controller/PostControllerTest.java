9302e5010d985039f133b23ec16aae94834d6a0b
ddgg9511@naver.com <ddgg9511@naver.com>
2022-03-22 23:57:16 +0900

    게시물 조회 테스트

--- book/src/test/java/com/example/book/controller/PostControllerTest.java
+++ book/src/test/java/com/example/book/controller/PostControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.book.controller;
 
+import com.example.book.domain.Posts;
 import com.example.book.domain.User;
+import com.example.book.dto.PostsSaveRequestData;
 import com.example.book.dto.SessionResponseData;
 import com.example.book.dto.UserLoginData;
 import com.example.book.dto.UserSaveRequestData;
@@ -9,6 +11,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,8 +27,16 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 //import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -69,6 +81,33 @@ class PostControllerTest {
         sessionResponseData = login(getLoginData(user));
     }
 
+    @Nested
+    @DisplayName("게시물 목록 조회 요청은")
+    class Describe_GET_List{
+        @Nested
+        @DisplayName("등록된 게시물이 있다면")
+        class Context_exist_root{
+            final int postCount = 10;
+            List<Posts> postsList = new ArrayList<>();
+
+            @BeforeEach
+            void setUp() throws Exception{
+                for(int i = 0; i < postCount; i++){
+                    Posts posts = preparePost(getPostSaveData());
+                    postsList.add(getPostSaveData().toEntity());
+                }
+            }
+
+            @Test
+            @DisplayName("전체 리스트를 리턴한다.")
+            void it_return_list() throws Exception{
+                ResultActions resultActions = mockMvc.perform(get("/posts"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(TITLE)));
+            }
+        }
+    }
+
     private User prepareUser(UserSaveRequestData saveRequestData) throws Exception {
         ResultActions actions = mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -104,5 +143,28 @@ class PostControllerTest {
                 .name("테스트이름")
                 .password("password123*")
                 .build();
+    }
+
+    private PostsSaveRequestData getPostSaveData() {
+        return PostsSaveRequestData.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .author(AUTHOR)
+                .email(EMAIL)
+                .build();
+    }
+
+    private Posts preparePost(PostsSaveRequestData saveRequestData) throws Exception {
+        ResultActions actions = mockMvc.perform(post("/posts")
+                .content(objectMapper.writeValueAsString(saveRequestData))
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization",
+                        "Bearer " + sessionResponseData.getAccessToken())
+        );
+
+        MvcResult mvcResult = actions.andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+
+        return objectMapper.readValue(content, Posts.class);
     }
 }
